e1c741a36013a831dea4361306378b7479037a04
Kannan Raveendran Nair <Kannan@Laptop>
2019-12-29 23:50:36 -0500

    Multiple changes made to make the project compatable  for Pega 8.3 Changes to be committed: modified:   PegaPRPC-WebApp/Dockerfile  >> Optimize Dockerfile to have fewer commands, and generic commands to tolerate missing .war files modified:   PegaPRPC-WebApp/resources/context.xml.tmpl  >> Modified to work for Pega 8.3. Added rules & data schema modified:   Postgresql-BackendDB/docker-entrypoint-initdb.d/50-restoredata.sh  >> Modified to work for Pega 8.3. Search path modified to use pega 8.3 restore dump tables modified:   docker-compose.yml  >> Modified to have generic pega name instead of Pega721

--- Postgresql-BackendDB/docker-entrypoint-initdb.d/50-restoredata.sh
+++ Postgresql-BackendDB/docker-entrypoint-initdb.d/50-restoredata.sh
@@ -8,7 +8,10 @@ if [ -f "$PEGA_DUMP" ];then
 
     #set search path so that pega web app's db operations without a schema is able to find the correct tables
     echo "Setting search path for database $POSTGRES_DB"
-    psql --command="ALTER USER $POSTGRES_USER SET SEARCH_PATH to \"\$user\",personaledition,public;" --echo-all --echo-queries
+    #set search path for Pega 8.3 Personal Edition
+    psql --command="ALTER USER $POSTGRES_USER SET SEARCH_PATH to \"\$user\",rules,data,public;" --echo-all --echo-queries
+    #set search path for Pega 7.2.1 Personal Edition
+    #psql --command="ALTER USER $POSTGRES_USER SET SEARCH_PATH to \"\$user\",personaledition,public;" --echo-all --echo-queries
 
     # restore contents from pega.dump into pega database
     echo "Restoring $RES_LOC/pega.dump... This might take few minutes"
